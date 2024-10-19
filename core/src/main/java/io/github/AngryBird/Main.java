5801b957dec70a69ef3b231cad7a6fcb7d94acae
harshit08042006 <147906270+harshit08042006@users.noreply.github.com>
2024-10-19 12:14:15 +0530

    Hello Added

--- core/src/main/java/io/github/AngryBird/Main.java
+++ core/src/main/java/io/github/AngryBird/Main.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
-
+    //Hello
 
     @Override
     public void create() { setScreen(new FirstScreen(this));
